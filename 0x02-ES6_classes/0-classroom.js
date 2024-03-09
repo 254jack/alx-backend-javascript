a83723321689a2941f173aa14b07b820f1f5852a
254jack <taixonj@gmail.com>
2024-03-09 08:37:55 +0300

    update

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,5 +1,7 @@
-export default class ClassRoom {
-  constructor(maxStudentSize) {
-    this._maxStudentsSize = maxStudentSize;
+class ClassRoom {
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
   }
 }
+
+export default ClassRoom;
