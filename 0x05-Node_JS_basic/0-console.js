c9e0d9d912d40831eccce424677bef3fea8a6879
254jack <taixonj@gmail.com>
2024-04-30 19:45:25 +0300

    first commit

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * A method which dispaly a message to the STDOUT
+ * @param {String} message message displayed
+ * @author Yidnekachew Bantrga <https://github.com/Yidne21>
+ */
+const displayMessage = (message) => {
+  console.log(message);
+};
+
+module.exports = displayMessage;
